268e9c0aafc3bbc604be683af8e5c35c54697663
Jordan Fearnley <fearnlj01@gmail.com>
2023-03-24 05:43:08 +0000

    Add (extremely) limited settings validation

--- Settings/IShokoSettings.cs
+++ Settings/IShokoSettings.cs
@@ -3,5 +3,5 @@ namespace Shoko.Plugin.WebhookDump.Settings;
 public interface IShokoSettings
 {
 	public string ApiKey { get; set; }
-	public string ServerPort { get; set; }
+	public int ServerPort { get; set; }
 }
--- Settings/ShokoSettings.cs
+++ Settings/ShokoSettings.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 namespace Shoko.Plugin.WebhookDump.Settings;
 
 public class ShokoSettings : IShokoSettings
 {
 	public string ApiKey { get; set; }
-	public string ServerPort { get; set; } = "8111";
+	
+	[Range(1, 65536, ErrorMessage = "A server port of no more than 65536 may be set.")]
+	public int ServerPort { get; set; } = 8111;
 }
