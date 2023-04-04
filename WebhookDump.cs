d424befb960aede22a1183ac7269dfc0d6d909d6
Jordan Fearnley <fearnlj01@gmail.com>
2023-04-04 22:45:53 +0100

    Feat: Track and edit webhooks It's cursed currently, but eventually it'll be done properly™

--- WebhookDump.cs
+++ WebhookDump.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -29,6 +31,8 @@ public class WebhookDump : IPlugin
 
   private readonly HashSet<int> seenFiles = new();
 
+  private readonly Dictionary<int, long> sentWebhooks = new();
+
   public static void ConfigureServices(IServiceCollection services)
   {
     services.AddSingleton<ICustomSettingsProvider, CustomSettingsProvider>();
@@ -38,6 +42,7 @@ public class WebhookDump : IPlugin
   public WebhookDump(IShokoEventHandler eventHandler, ICustomSettingsProvider settingsProvider)
   {
     eventHandler.FileNotMatched += OnFileNotMatched;
+    eventHandler.FileMatched += OnFileMatched;
     _settingsProvider = (CustomSettingsProvider)settingsProvider;
     _settings = _settingsProvider.GetSettings();
   }
@@ -49,6 +54,9 @@ public class WebhookDump : IPlugin
   public void Load()
   {
     _logger.Info($"Loaded (custom) settings without a string representation: {_settings}");
+    #region TestData
+    _  = "{\"id\":\"1092536396230705152\",\"type\":0,\"content\":\"\",\"channel_id\":\"1058301714924589096\",\"author\":{\"bot\":true,\"id\":\"1058302498122760233\",\"username\":\"Shoko\",\"avatar\":\"ef2254e105a3d1d4a9bfd6ad6c0935d8\",\"discriminator\":\"0000\"},\"attachments\":[],\"embeds\":[{\"type\":\"rich\",\"url\":\"http://localhost:8111/webui/utilities/unrecognized/files\",\"title\":\"[SuPlease] N1 - 12 80p) [D08C0F8D].mkv\",\"description\":\"The above file has been found by Shoko Server but could not be matched against AniDB. The file has now been dumped with AVDump, result as below.\",\"color\":3900150,\"fields\":[{\"name\":\"ED2K:\",\"value\":\"ed2k://|file|[SuPlease] N1 - 12 80p) [D08C0F8D].mkv|378382638|7601386EB3885661D5BFD370E7612E2D|/\",\"inline\":false},{\"name\":\"AniDB Link\",\"value\":\"[Fate/Stay Night](https://anidb.net/anime/3348/release/add)\",\"inline\":true},{\"name\":\"AniDB Link\",\"value\":\"[K-On!](https://anidb.net/anime/6257/release/add)\",\"inline\":true},{\"name\":\"AniDB Link\",\"value\":\"[City Hunter](https://anidb.net/anime/942/release/add)\",\"inline\":true}] }],\"mentions\":[],\"mention_roles\":[],\"pinned\":false,\"mention_everyone\":false,\"tts\":false,\"timestamp\":\"2023-04-03T19:49:35.206000+00:00\",\"edited_timestamp\":null,\"flags\":0,\"components\":[],\"webhook_id\":\"1058302498122760233\"}";
+    #endregion
   }
 
   private async void OnFileNotMatched(object sender, FileNotMatchedEventArgs fileNotMatchedEvent)
@@ -79,25 +87,69 @@ public class WebhookDump : IPlugin
 
         try
         {
-          HttpRequestMessage request = new(HttpMethod.Post, url)
+          HttpRequestMessage request = new(HttpMethod.Post, $"{url}?wait=true")
           {
             Content = new StringContent(json, Encoding.UTF8, "application/json")
           };
           var response = await _httpClient.SendAsync(request);
           response.EnsureSuccessStatusCode();
+
+          var content = await response.Content.ReadAsStringAsync();
+          var jsonRoot = JsonDocument.Parse(content).RootElement;
+
+          if (!jsonRoot.TryGetProperty("id", out var messageIdProp)) return;
+          if (!long.TryParse(messageIdProp.GetString(), out var messageId)) return;
+          sentWebhooks.Add(fileInfo.VideoFileID, messageId);
         }
         catch (HttpRequestException e)
         {
           _logger.Error("Webhook failed to send!", e);
         }
         break;
-      case <= 4:
+      case <= 5:
         if (!seenFiles.Contains(fileInfo.VideoFileID)) break;
         _ = Task.Run(() => RescanFile(fileInfo, matchAttempts));
         break;
       default:
         break;
     }
+  }
+
+  private async void OnFileMatched(object sender, FileMatchedEventArgs fileMatchedEventArgs)
+  {
+
+    var fileInfo = fileMatchedEventArgs.FileInfo;
+    var shokoId = fileInfo.VideoFileID;
+    if (!seenFiles.Remove(shokoId)) return;
+    if (!sentWebhooks.TryGetValue(shokoId, out long webhookMessageId)) return;
+
+    var episodeInfo = fileMatchedEventArgs.EpisodeInfo.FirstOrDefault();
+    var animeInfo = fileMatchedEventArgs.AnimeInfo.FirstOrDefault();
+
+    var episodeTitle = episodeInfo.Titles.Where(t => t.Language == TitleLanguage.English).FirstOrDefault();
+    var episodeNum = episodeInfo.Number.ToString("00", CultureInfo.InvariantCulture);
+    var shokoUrl = $"{_settings.Shoko.PublicUrl}:{_settings.Shoko.PublicPort}".TrimEnd(':');
+
+    var json = $$"""{"content": null, "embeds": [{"title": "{{fileInfo.Filename}}", "description": "An unmatched file automatically dumped by this plugin has now been matched.", "url": "{{shokoUrl}}", "color": {{0x57f287}}, "fields": [{"name": "Entry", "value": "{{episodeNum}} - [{{episodeTitle.Title}}](https://anidb.net/e{{episodeInfo.EpisodeID}})", "inline": true }, {"name": "File ID", "value": "{{fileInfo.VideoFileID}}", "inline": true }, {"name": "Anime", "value": "[{{animeInfo.PreferredTitle}}](https://anidb.net/a{{animeInfo.AnimeID}})", "inline": false }]}], "username": "Shoko", "avatar_url": "{{_settings.Webhook.AvatarUrl}}", "attachments": []}""";
+
+    var url = $"{_settings.Webhook.Url}/messages/{webhookMessageId}";
+    HttpRequestMessage request = new(HttpMethod.Patch, url)
+    {
+      Content = new StringContent(json, Encoding.UTF8, "application/json")
+    };
+
+    try
+    {
+      var response = await _httpClient.SendAsync(request);
+      var debugOutput = response.StatusCode;
+      response.EnsureSuccessStatusCode();
+    }
+    catch (HttpRequestException)
+    {
+      _logger.Warn($"Couldn't edit webhook for file: \"{fileInfo.Filename}\"");
+    }
+
+    sentWebhooks.Remove(shokoId);
   }
 
   private async Task<AVDumpResult> DumpFile(IVideoFile file, int attemptCount = 1)
