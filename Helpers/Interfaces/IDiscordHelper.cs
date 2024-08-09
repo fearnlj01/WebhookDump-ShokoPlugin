551a511a82d1f55182f3a93f8bf905eaeeb0431a
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2024-08-09 12:52:01 +0530

    Update `Shoko.Plugin.Abstractions` to `3.0.0-alpha11`

--- Helpers/Interfaces/IDiscordHelper.cs
+++ Helpers/Interfaces/IDiscordHelper.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using Shoko.Plugin.Abstractions.DataModels;
+using Shoko.Plugin.Abstractions.DataModels.Shoko;
 using Shoko.Plugin.WebhookDump.Models.AniDB;
 
 namespace Shoko.Plugin.WebhookDump.Apis;
@@ -8,7 +9,7 @@ namespace Shoko.Plugin.WebhookDump.Apis;
 public interface IDiscordHelper
 {
   void Dispose();
-  Task PatchWebhook(IVideoFile file, IAnime anime, IEpisode episode, MemoryStream imageStream, string messageId);
+  Task PatchWebhook(IVideoFile file, IShokoSeries anime, IEpisode episode, MemoryStream imageStream, string messageId);
   Task<string> SendWebhook(IVideoFile file, string dumpResult, AniDBSearchResult searchResult);
   Task<bool> GetMessageReactionState(string messageId);
 }
