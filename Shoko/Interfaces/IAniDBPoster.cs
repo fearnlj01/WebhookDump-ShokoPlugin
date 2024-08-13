2e22535b13799ea730406144303e327ac6fb15ba
Jordan Fearnley <fearnlj01@gmail.com>
2024-08-13 23:11:56 +0100

    Fix: (de)serialize image IDs as int's not strings

--- Shoko/Interfaces/IAniDBPoster.cs
+++ Shoko/Interfaces/IAniDBPoster.cs
@@ -2,9 +2,9 @@ namespace Shoko.Plugin.WebhookDump.Models.AniDB;
 
 public interface IAniDBPoster
 {
+  public int ID { get; set; }
   public ImageSource Source { get; set; }
   public ImageType Type { get; set; }
-  public string ID { get; set; }
   public string RelativeFilePath { get; set; }
   public bool Preferred { get; set; }
   public int? Width { get; set; }
